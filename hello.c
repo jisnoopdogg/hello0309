30a5238c7682979088ec337a8e28a83e788d1955
jinsoo <jspark837@gmail.com>
2026-03-13 17:51:23 +0900

    date calculator

--- hello.c
+++ hello.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    int month, day; 		// 월, 일
-    int day_count; 			// 1년 중 날 수 
+    int m, day; 		// 월, 일
+    int day_count=0; 			// 1년 중 날 수 
 
     printf("날짜를 입력해주세요: ");
-    scanf("%d %d", month, day);
+    scanf("%d %d", &m, &day);
 
-    for(int i=0;i<month;i++){
-        if(month == 2){
+    for(int i=1;i<m;i++){
+        if(i == 2){
             day_count = day_count + 28;
-        }else if(month == 1||3||5||7||8||10||12){
+        }else if(i == 1||i==3||i==5||i==7||i==8||i==10||i==12){
             day_count = day_count + 31;
-        }else if(month == 4||6||9||11){
+        }else if(i == 4||i==6||i==9||i==11){
             day_count = day_count + 30;
         }else{
             continue;
@@ -24,6 +24,6 @@ int main(){
     }
 
     printf("%d",day_count);
-    
+
     return 0;
 }
